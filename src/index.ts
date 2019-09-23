93cbb3cf22f0f8d13d7e8ce0128655696d8b0aac
Barrior <barrior@qq.com>
2019-09-23 12:52:41 +0800

    refactor: with class

--- src/index.ts
+++ src/index.ts
@@ -23,66 +23,55 @@ export interface IObjectAny {
   [key: string]: any
 }
 
-function handleOptions(options: string[] | IOptions) {
-  let suffix: string = modifiedAt.suffix
-  let fields: string[] = []
-  let select: boolean = true
-  const customList: {
-    [key: string]: (doc: IObjectAny) => boolean | undefined | null
-  } = {}
-
-  if (isArray(options)) {
-    fields = options
-  } else if (isPlainObject(options)) {
-    if (isString(options.suffix)) {
-      suffix = options.suffix
-    }
-
-    if (isBoolean(options.select)) {
-      select = options.select
-    }
-
-    if (isArray(options.fields)) {
-      fields = options.fields
-    }
-
-    forEach(omit(options, ['suffix', 'select', 'fields']), (value, key) => {
-      if (isFunction(value)) {
-        customList[key] = value
-      }
-    })
-  } else {
-    throw Error('Missing options or type error of parameter "options"')
-  }
-
-  return { suffix, select, fields, customList }
+export interface ICustomList {
+  [key: string]: (doc: IObjectAny) => boolean | undefined | null
 }
 
-function modifiedAt(schema: any, options: string[] | IOptions): void {
-  const { suffix, select, fields, customList } = handleOptions(options)
+export type ICorrectOptions = Required<IOptions> & {
+  customList: ICustomList
+}
 
-  function addTimeFieldToSchema(pathname: string): void {
-    schema.add({
+const pluginName: string = 'modifiedAt'
+
+class ModifiedAt {
+  constructor(
+    public readonly schema: any,
+    public readonly options: ICorrectOptions
+  ) {
+    this.schema = schema
+    this.options = options
+    this.initializeFieldSchema()
+    this.addHooks()
+  }
+
+  public addPathToSchema(pathname: string) {
+    const { select } = this.options
+    this.schema.add({
       [pathname]: { type: Date, select },
     })
   }
 
-  // Add schema for every field
-  forEach(fields, field => {
-    addTimeFieldToSchema(field + suffix)
-  })
+  public initializeFieldSchema() {
+    const { suffix, fields, customList } = this.options
 
-  // tslint:disable-next-line:variable-name
-  forEach(customList, (_value, pathname) => {
-    addTimeFieldToSchema(pathname)
-  })
+    // Add schema for every field
+    forEach(fields, field => {
+      this.addPathToSchema(field + suffix)
+    })
 
-  async function setTimestamps(params: {
+    // tslint:disable-next-line:variable-name
+    forEach(customList, (_value, pathname) => {
+      this.addPathToSchema(pathname)
+    })
+  }
+
+  public async setTimestamps(params: {
     that?: any
     purelySet?: boolean
     doc: any
     modifiedPaths: string[]
   }) {
+    const { suffix, fields, customList } = this.options
     const purelySet = params.hasOwnProperty('purelySet')
       ? params.purelySet
       : true
@@ -115,65 +104,106 @@ function modifiedAt(schema: any, options: string[] | IOptions): void {
     })
   }
 
-  // for Document
-  // tslint:disable-next-line:variable-name
-  schema.pre('save', async function(this: any, _next: any, opts: any) {
-    if (get(opts, 'modifiedAt') === false) {
-      return
-    }
-    await setTimestamps({
-      that: this,
-      doc: this,
-      modifiedPaths: this.modifiedPaths(),
-    })
-  })
+  public addHooks() {
+    const $this = this
 
-  // for Query
-  const updateHooks = ['findOneAndUpdate', 'update', 'updateOne', 'updateMany']
-  schema.pre(updateHooks, async function(this: any) {
-    const opts = this.getOptions()
-    if (opts.modifiedAt === false) {
-      return
-    }
-    const updates = this.getUpdate()
-    await setTimestamps({
-      that: this,
-      doc: assign({}, this.getFilter(), updates),
-      modifiedPaths: keys(updates),
+    // for Document
+    // tslint:disable-next-line:variable-name
+    this.schema.pre('save', async function(this: any, _next: any, opts: any) {
+      if (get(opts, pluginName) === false) {
+        return
+      }
+      await $this.setTimestamps({
+        that: this,
+        doc: this,
+        modifiedPaths: this.modifiedPaths(),
+      })
     })
-  })
 
-  // for Query
-  const replaceHooks = ['findOneAndReplace', 'replaceOne']
-  schema.pre(replaceHooks, async function(this: any) {
-    const opts = this.getOptions()
-    if (opts.modifiedAt === true) {
-      const updates = JSON.parse(JSON.stringify(this.getUpdate()))
-      await setTimestamps({
-        that: updates,
+    // for Query
+    // prettier-ignore
+    const updateHooks = ['findOneAndUpdate', 'update', 'updateOne', 'updateMany']
+    this.schema.pre(updateHooks, async function(this: any) {
+      if (get(this.getOptions(), pluginName) === false) {
+        return
+      }
+      const updates = this.getUpdate()
+      await $this.setTimestamps({
+        that: this,
         doc: assign({}, this.getFilter(), updates),
         modifiedPaths: keys(updates),
-        purelySet: false,
       })
-      this.setUpdate(updates)
-    }
-  })
+    })
 
-  // for Model
-  // tslint:disable-next-line:variable-name only-arrow-functions
-  schema.pre('insertMany', async function(_next: any, docs: any[], opts: any) {
-    if (get(opts, 'modifiedAt') === false) {
-      return
+    // for Query
+    const replaceHooks = ['findOneAndReplace', 'replaceOne']
+    this.schema.pre(replaceHooks, async function(this: any) {
+      if (get(this.getOptions(), pluginName) === true) {
+        const updates = JSON.parse(JSON.stringify(this.getUpdate()))
+        await $this.setTimestamps({
+          that: updates,
+          doc: assign({}, this.getFilter(), updates),
+          modifiedPaths: keys(updates),
+          purelySet: false,
+        })
+        this.setUpdate(updates)
+      }
+    })
+
+    // for Model
+    // prettier-ignore
+    // tslint:disable-next-line:variable-name only-arrow-functions
+    this.schema.pre('insertMany', async function(_next: any, docs: any[], opts: any) {
+      if (get(opts, pluginName) === false) {
+        return
+      }
+      for (const doc of docs) {
+        await $this.setTimestamps({
+          that: doc,
+          doc,
+          modifiedPaths: keys(doc),
+          purelySet: false,
+        })
+      }
+    })
+  }
+}
+
+function handleOptions(options: string[] | IOptions): ICorrectOptions {
+  let suffix: string = modifiedAt.suffix
+  let fields: string[] = []
+  let select: boolean = true
+  const customList: ICustomList = {}
+
+  if (isArray(options)) {
+    fields = options
+  } else if (isPlainObject(options)) {
+    if (isString(options.suffix)) {
+      suffix = options.suffix
     }
-    for (const doc of docs) {
-      await setTimestamps({
-        that: doc,
-        doc,
-        modifiedPaths: keys(doc),
-        purelySet: false,
-      })
+
+    if (isBoolean(options.select)) {
+      select = options.select
     }
-  })
+
+    if (isArray(options.fields)) {
+      fields = options.fields
+    }
+
+    forEach(omit(options, ['suffix', 'select', 'fields']), (value, key) => {
+      if (isFunction(value)) {
+        customList[key] = value
+      }
+    })
+  } else {
+    throw Error('Missing options or type error of parameter "options"')
+  }
+
+  return { suffix, select, fields, customList }
+}
+
+function modifiedAt(schema: any, options: string[] | IOptions) {
+  return new ModifiedAt(schema, handleOptions(options))
 }
 
 modifiedAt.suffix = '_modifiedAt'
